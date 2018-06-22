579a5485d5f25e6a104d0f739536cec36129fcc3
Anthony Denicolo <denicoloa@live.bucks.edu>
2018-06-22 14:22:30 -0400

    figured out how to split the string into substrings to figure out which substrings are longer

---
+++ App2/js/main.js
@@ -0,0 +1,16 @@
+﻿var list = new Array();
+var string = "hello world";
+var splitstring=string.split(" ");
+console.log(splitstring[0]);
+
+
+function blah(derp) {
+    for (var i = 0; i <= derp.length ;i++) {
+      list.push(i);
+      console.log(list[i]);
+    };
+  
+    
+};
+// blah("hello world");
+
